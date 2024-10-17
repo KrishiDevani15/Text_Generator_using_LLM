4b675b074e21a2706938f278eafc19b5bcd4a82d
“KrishiDevani15” <krishiminergate@gmail.com>
2024-10-17 18:27:20 +0530

    Deployment and small upate footer and favicon icon added

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from langchain.prompts.prompt import PromptTemplate  # type: ignore
 from langchain_ollama import ChatOllama  # type: ignore
 from langchain_core.output_parsers import StrOutputParser # type: ignore
 
+
+st.set_page_config(
+    page_title="Text Genarator",
+    page_icon="favicon.ico",  # Replace with your favicon path
+    layout="centered"  # or "wide" depending on your preference
+)
+
+
 st.title("Text Generation App 🤖✍️")
 
 user_prompt = st.text_area("Enter your prompt:",height=300)
@@ -27,3 +35,16 @@ if st.button("Generate Text"):
         st.balloons()
     else:
         st.warning("Please enter a prompt to generate text.")
+
+st.markdown(
+    """
+    <footer style="text-align: center; margin-top: 20px; font-size: 14px;">
+        <a href="https://whoiskrishi.vercel.app/" target="_blank" style="text-decoration: none; color: white;">
+            <br>
+            © 2024 @whoisKrishi — KRISHI DEVANI
+        </a>
+        
+    </footer>
+    """,
+    unsafe_allow_html=True
+)
